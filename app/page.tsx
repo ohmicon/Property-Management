9a41a970fa8fa9bc1a3cf66dc89b955ddd6af244
Poohcid Rugrawi <111724148+poohcidicon@users.noreply.github.com>
2025-10-09 17:25:19 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,38 @@
+"use client"
+
+import { useEffect, useState } from 'react';
 import PropertyLayout from "../app/property-layout/page"
 
-interface HomePageProps {
-  params: { id: string }; // Type the `id` parameter as a string
-  searchParams: Promise<{ 
-    p: string;
-    type: string;
-  }>; // Search params are optional
-}
+// interface HomePageProps {
+//   params: { id: string }; // Type the `id` parameter as a string
+//   searchParams: Promise<{ 
+//     p: string;
+//     type: string;
+//   }>; // Search params are optional
+// }
 
 const types = ['market', 'hotel']
-export default async function HomePage({ searchParams }: HomePageProps) {
-
+export default function HomePage() {
   // ใส่ Default
-  const typeBusiness = (await searchParams).type || 'market';
-  const projectId = (await searchParams).p || 'M004'
-  if (typeof typeBusiness === 'string' && types.includes(typeBusiness)) {
+  // const typeBusiness = (await searchParams).type || 'market';
+  // const typeBusiness = 'market'
+  // const projectId = (await searchParams).p || 'M004'
+  // const projectId = 'M004'
+  const [typeBusiness, setTypeBusiness] = useState<string | null>()
+  const [projectId, setProjectId] = useState<string | null>()
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const selectTypeBusiness = params.get('type');
+    const selectProject = params.get('p')
+    setTypeBusiness(selectTypeBusiness || 'market')
+    setProjectId(selectProject || 'M004')
+  }, [])
+
+  if (typeof typeBusiness === 'string' && types.includes(typeBusiness) && projectId) {
     return <PropertyLayout typeBusiness={typeBusiness} projectId={projectId}/>
   }
-  return <></>
-}
 
-export async function generateStaticParams() {
-  return [
-    { p: 'M004', type: 'market' },
-    { id: '2', type: 'market' },
-    { id: '3', type: 'market' },
-  ];
+  
+  return <></>
 }
